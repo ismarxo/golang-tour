35e700ecd98431846d0ff7109e673444469b8774
Ildar Sadykov <ismarxovich@gmail.com>
2023-02-19 19:12:53 +0400

    add: test of inference of lang

---
+++ basics/type_inference.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := ""
+	fmt.Printf("s is of type %T\n", s)
+
+	b := true
+	fmt.Printf("b is of type %T\n", b)
+
+	i := 1
+	fmt.Printf("i is of type %T\n", i)
+}
